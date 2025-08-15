169eafe7210c7b7c011c9575be0763ef58a85ea6
farazalam2017 <farazalam2017@gmail.com>
2025-08-15 22:43:55 +0530

    react

---
+++ 03-pizza-menu/starter/project/src/App.jsx
@@ -0,0 +1,128 @@
+import React from "react";
+import "./App.css";
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+};
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We are still working on our menu Please come later.</p>
+      )}
+      {/*  <Pizza
+        name="fmkmm"
+        ingredients="hfj jjfjf jjfj"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="jj "
+        ingredients="hjhj jhjhjj jhjh"
+        photoName="pizzas/funghi.jpg"
+        price={12}
+      /> */}
+    </main>
+  );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour < closeHour;
+  console.log(isOpen);
+  if (!isOpen) return <p>Closed</p>;
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <div className="order">
+          <p>We are open until {closeHour}:00. Come visit us order online</p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>We are happy to welcome you</p>
+      )}
+    </footer>
+  );
+}
+function Pizza({ pizzaObj }) {
+  // console.log(props);
+  if (pizzaObj.soldOut) return null;
+  return (
+    <li className="pizza">
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <div>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price + 3}</span>
+      </div>
+    </li>
+  );
+}
+export default App;
--- README.md
+++ README.md
@@ -2,3 +2,30 @@
 
 This section documents my learning journey from the Udemy course:  
 [The Ultimate React Course 2025: React, Next.js, Redux, and More](https://www.udemy.com/course/the-ultimate-react-course/) by Jonas Schmedtmann.
+
+### Important links to explore
+
+- https://react.dev/?ref=jonas.io
+  Desc:- react official documentation
+- https://vite.dev/guide/?ref=jonas.io
+  Desc:- Vite official documentation
+- https://gist.githubusercontent.com/gaearon/0275b1e1518599bbeafcde4722e79ed1/raw/db72dcbf3384ee1708c4a07d3be79860db04bff0/example.html
+  Desc:- embedding react script in html (old pure form)
+
+### Important tools, plugins, extension to become good react developer
+
+## Practice
+
+### Code Sandbox demo
+
+1. https://codesandbox.io/p/sandbox/ecstatic-cray-n8hj4c
+   Description:- simple react app to get random advice by clicking a button and simultaneoulsy counting the generated adivce by 3rd party API
+2. https://codesandbox.io/p/sandbox/react-first-app-advice-52879f?file=%2Fsrc%2Fvanilla-JS.html
+   Desc:- same as above but using vanilla js instead of react, original file by jonas Schmedtmann
+3. https://codesandbox.io/p/sandbox/dreamy-herschel-glrpg7 :- Profile card-v1
+   Desc:- Simple assignment to demonstrate passing of props and using components
+4. https://codesandbox.io/p/sandbox/infallible-napier-pqtdxy :- profile card-v2
+   Desc:= this project is 2.0 version of Profile card-v1, where we have refactored our code by including different features such as redering list using map, conditional rendering, destrcuturing props, extracting JSX into components.
+5.
+
+### Projects
